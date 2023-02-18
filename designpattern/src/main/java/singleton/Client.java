a127db6a73f7fdf846d5aa3f41a8e0517f3c987c
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2023-02-18 23:07:52 +0800

    singleton

---
+++ designpattern/src/main/java/singleton/Client.java
@@ -0,0 +1,28 @@
+package singleton;
+
+public class Client {
+    public static void main(String[] args) {
+        Thread ta = new Thread(new ThreadA());
+        Thread tb = new Thread(new ThreadB());
+        ta.start();
+        tb.start();
+    }
+
+    static class ThreadA implements Runnable {
+
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("A");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadB implements Runnable {
+
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("B");
+            System.out.println(singleton.value);
+        }
+    }
+}
