3bd29e058ef870278022c3f91415177de777e86f
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2023-02-27 00:13:08 +0800

    design pattern: mediator

---
+++ designpattern/src/main/java/behavioral/mediator/components/AddButton.java
@@ -0,0 +1,31 @@
+package behavioral.mediator.components;
+
+import behavioral.mediator.mediator.Mediator;
+import behavioral.mediator.mediator.Note;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class AddButton extends JButton implements Component {
+    private Mediator mediator;
+
+    public AddButton() {
+        super("Add");
+    }
+
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    protected void fireActionPerformed(ActionEvent event) {
+        mediator.addNewNote(new Note());
+    }
+
+    @Override
+    public String getName() {
+        return "AddButton";
+    }
+
+}
