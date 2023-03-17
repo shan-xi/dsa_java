d49940bea3c82666ed3f4dfcb6bd711261537ce0
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2023-03-17 12:00:10 +0800

    leetcode: 383. Ransom Note

---
+++ leetcode/src/main/java/count_sub_islands/Solution.java
@@ -0,0 +1,9 @@
+package count_sub_islands;
+
+// 1905. Count Sub Islands
+// https://leetcode.com/problems/count-sub-islands/?envType=study-plan&id=graph-i
+public class Solution {
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        return 0;
+    }
+}
---
+++ leetcode/src/test/java/count_sub_islands/SolutionTest.java
@@ -0,0 +1,26 @@
+package count_sub_islands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    public void test_case1_success() {
+        Solution s = new Solution();
+        int[][] grid1 = {{1, 1, 1, 0, 0}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 1, 1}};
+        int[][] grid2 = {{1, 1, 1, 0, 0}, {0, 0, 1, 1, 1}, {0, 1, 0, 0, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}};
+        int a = s.countSubIslands(grid1, grid2);
+        int e = 3;
+        Assertions.assertEquals(e, a);
+    }
+
+    @Test
+    public void test_case2_success() {
+        Solution s = new Solution();
+        int[][] grid1 = {{1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}};
+        int[][] grid2 = {{0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 0}, {1, 0, 0, 0, 1}};
+        int a = s.countSubIslands(grid1, grid2);
+        int e = 2;
+        Assertions.assertEquals(e, a);
+    }
+}
