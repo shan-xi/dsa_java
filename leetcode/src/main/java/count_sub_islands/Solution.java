ad4255161d61318db7258000d8488b444f5a1e0d
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2023-03-17 15:30:39 +0800

    leetcode: 1905. Count Sub Islands

--- leetcode/src/main/java/count_sub_islands/Solution.java
+++ leetcode/src/main/java/count_sub_islands/Solution.java
@@ -3,7 +3,42 @@ package count_sub_islands;
 // 1905. Count Sub Islands
 // https://leetcode.com/problems/count-sub-islands/?envType=study-plan&id=graph-i
 public class Solution {
-    public int countSubIslands(int[][] grid1, int[][] grid2) {
-        return 0;
+    private class Validation {
+        boolean isValid = true;
     }
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int rows = grid1.length;
+        int cols = grid1[0].length;
+        int count = 0;
+        Validation v = new Validation();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid2[i][j] == 1) {
+                    v.isValid = true;
+                    dfs(grid1, grid2, rows, cols, i, j, v);
+                    if (v.isValid)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int[][] grid1, int[][] grid2, int rows, int cols, int i, int j, Validation valid) {
+        if (grid1[i][j] != grid2[i][j]) {
+            grid2[i][j] = 0;
+            valid.isValid = false;
+        }
+        grid2[i][j] = 0;
+        if (i - 1 >= 0 && grid2[i - 1][j] == 1)
+            dfs(grid1, grid2, rows, cols, i - 1, j, valid);
+        if (i + 1 < rows && grid2[i + 1][j] == 1)
+            dfs(grid1, grid2, rows, cols, i + 1, j, valid);
+        if (j - 1 >= 0 && grid2[i][j - 1] == 1)
+            dfs(grid1, grid2, rows, cols, i, j - 1, valid);
+        if (j + 1 < cols && grid2[i][j + 1] == 1)
+            dfs(grid1, grid2, rows, cols, i, j + 1, valid);
+    }
+
 }
