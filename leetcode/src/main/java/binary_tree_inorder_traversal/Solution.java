fe3d10b6c536ab16c24079625ba218299052145e
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2023-03-25 17:41:33 +0800

    leetcode: 94. Binary Tree Inorder Traversal

--- leetcode/src/main/java/binary_tree_inorder_traversal/Solution.java
+++ leetcode/src/main/java/binary_tree_inorder_traversal/Solution.java
@@ -9,25 +9,17 @@ import java.util.Stack;
 // 94. Binary Tree Inorder Traversal
 // https://leetcode.com/problems/binary-tree-inorder-traversal/
 public class Solution {
+    List<Integer> result = new ArrayList<>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-
-        Stack<TreeNode> stack = new Stack<>();
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        TreeNode current = root;
-        while (!stack.isEmpty() || current != null) {
-            if (current != null) {
-                stack.push(current);
-                current = current.left;
-            } else {
-                current = stack.pop();
-                result.add(current.val);
-                current = current.right;
-            }
-        }
-
+        inOrder(root);
         return result;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        result.add(root.val);
+        inOrder(root.right);
     }
 }
