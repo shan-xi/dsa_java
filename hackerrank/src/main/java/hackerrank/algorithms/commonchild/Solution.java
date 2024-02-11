7632e2573819d5a700b7c0242c208dab809c2b22
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-02-11 18:44:27 +0800

    Hackerrank: Common Child

---
+++ hackerrank/src/main/java/hackerrank/algorithms/commonchild/Solution.java
@@ -0,0 +1,57 @@
+package hackerrank.algorithms.commonchild;
+
+import java.io.IOException;
+
+class Result {
+
+    /*
+     * Complete the 'commonChild' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int commonChild(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int result = Result.commonChild(
+                "HARRY",
+                "SALLY");
+        System.out.println(result);
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        String s1 = bufferedReader.readLine();
+//
+//        String s2 = bufferedReader.readLine();
+//
+//        int result = Result.commonChild(s1, s2);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+}
