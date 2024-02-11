9d1f50ebf7081ff78ccd1f02686bd2a9819acf7e
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-02-11 14:40:58 +0800

    Hackerrank: Flipping the Matrix

---
+++ hackerrank/src/main/java/hackerrank/algorithms/flippingthematrix/Solution.java
@@ -0,0 +1,74 @@
+package hackerrank.algorithms.flippingthematrix;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'flippingMatrix' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        int totalSum = 0;
+        int n = matrix.size() / 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int upperLeft = matrix.get(i).get(j);
+                int bottomLeft = matrix.get(2 * n - 1 - i).get(j);
+                int upperRight = matrix.get(i).get(2 * n - 1 - j);
+                int bottomRight = matrix.get(2 * n - 1 - i).get(2 * n - 1 - j);
+                totalSum += Math.max(upperLeft, Math.max(bottomLeft, Math.max(upperRight, bottomRight)));
+            }
+        }
+        return totalSum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<List<Integer>> matrix = new ArrayList<>();
+
+                IntStream.range(0, 2 * n).forEach(i -> {
+                    try {
+                        matrix.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                int result = Result.flippingMatrix(matrix);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
