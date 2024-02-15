021e9389cb98a613a57ca6fc07ddde2f466343bf
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-02-15 21:41:17 +0800

    Hackerrank: Gaming Array

---
+++ hackerrank/src/main/java/hackerrank/algorithms/gaming_array/Solution.java
@@ -0,0 +1,68 @@
+package hackerrank.algorithms.gaming_array;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gamingArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static String gamingArray(List<Integer> arr) {
+        int max = 0; // keeps track of the maximum value seen so far
+        int moves = 0; // keeps track of the number of moves made
+
+        for (Integer integer : arr) {
+            if (integer > max) {
+                max = integer;
+                moves++; // player made a move, increment moves count
+            }
+        }
+
+        // If the total number of moves made by the first player is odd, the first player wins.
+        // Otherwise, the second player wins.
+        if (moves % 2 == 1) {
+            return "BOB";
+        } else {
+            return "ANDY";
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int g = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, g).forEach(gItr -> {
+            try {
+                int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = Result.gamingArray(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
