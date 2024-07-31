7ad77ae68bf1864bd7dd01ed8a7303af3ea774b6
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-07-31 16:39:42 +0800

    leetcode: 349_Intersection_of_Two_Arrays

---
+++ leetcode/src/main/java/topic/hash_table/_349_Intersection_of_Two_Arrays/Solution.java
@@ -0,0 +1,45 @@
+package topic.hash_table._349_Intersection_of_Two_Arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test case 1
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result1 = solution.intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result1)); // Output: [2]
+
+        // Test case 2
+        int[] nums3 = {4, 9, 5};
+        int[] nums4 = {9, 4, 9, 8, 4};
+        int[] result2 = solution.intersection(nums3, nums4);
+        System.out.println(Arrays.toString(result2)); // Output: [4, 9]
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> intersectionSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+
+        int[] result = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
