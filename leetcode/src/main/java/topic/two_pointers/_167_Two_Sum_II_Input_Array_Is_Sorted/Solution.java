3f0ca8841ca0b5bddd69944f9e52e913f0bcb720
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-08-13 14:49:22 +0800

    leetcode: 167. Two Sum II - Input Array Is Sorted

---
+++ leetcode/src/main/java/topic/two_pointers/_167_Two_Sum_II_Input_Array_Is_Sorted/Solution.java
@@ -0,0 +1,22 @@
+package topic.two_pointers._167_Two_Sum_II_Input_Array_Is_Sorted;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};  // This line should never be reached as per the problem constraints.
+    }
+}
